65ecb4e4fccbf495da23e37f079e7339cd544f0f
MasamichiKikuchi2023 <tomcat.mk58@gmail.com>
2024-03-10 22:43:17 +0900

    プレイヤーの挙動を調整

--- program.py
+++ program.py
@@ -10,8 +10,8 @@ def key_down(e):
     koff = False
 
 def key_up(e):
-    global koff
-    koff = True
+    global key
+    key = ""
 
 DIR_UP = 0
 DIR_DOWN = 1
@@ -23,13 +23,13 @@ player_y = 90
 
 map_data = [
     [0,1,1,1,1,1,1,1,1,1,1,0],
+    [0,3,2,2,2,2,2,2,2,2,3,0],
     [0,2,2,2,2,2,2,2,2,2,2,0],
     [0,2,2,2,2,2,2,2,2,2,2,0],
     [0,2,2,2,2,2,2,2,2,2,2,0],
     [0,2,2,2,2,2,2,2,2,2,2,0],
     [0,2,2,2,2,2,2,2,2,2,2,0],
-    [0,2,2,2,2,2,2,2,2,2,2,0],
-    [0,2,2,2,2,2,2,2,2,2,2,0],
+    [0,3,2,2,2,2,2,2,2,2,3,0],
     [0,1,1,1,1,1,1,1,1,1,1,0]
 ]
 
@@ -43,53 +43,64 @@ def draw_screen():
     canvas.create_image(player_x,player_y,image=img_player,tag="SCREEN")
 
 #キャラクターの移動方向に壁があるか調べる
-def check_wall(cx,cy,di):
+def check_wall(cx,cy,di,dot):
     check = False
     if di == DIR_UP:
-        mx = int(cx/60)
-        my = int((cy-60)/60)
+        mx = int((cx-30)/60)
+        my = int((cy-30-dot)/60)
         if map_data[my][mx] <= 1:
             check = True
+        mx = int((cx+29)/60)
+        if map_data[my][mx] <= 1:
+            chk = True
     if di == DIR_DOWN:
-        mx = int(cx/60)
-        my = int((cy+60)/60)
+        mx = int((cx-30)/60)
+        my = int((cy+29+dot)/60)
         if map_data[my][mx] <= 1:
             check = True
+        mx = int((cx+29)/60)
+        if map_data[my][mx] <= 1:
+            check = True   
     if di == DIR_LEFT:
-        mx = int((cx-60)/60)
-        my = int(cy/60)
+        mx = int((cx-30-dot)/60)
+        my = int((cy-30)/60)
+        if map_data[my][mx] <= 1:
+            check = True
+        mx = int((cy+29)/60)
         if map_data[my][mx] <= 1:
             check = True
     if di == DIR_RIGHT:
-        mx = int((cx+60)/60)
-        my = int(cy/60)
+        mx = int((cx+29+dot)/60)
+        my = int((cy-30)/60)
         if map_data[my][mx] <= 1:
-            check = True                
+            check = True
+        mx = int((cx+29)/60)
+        if map_data[my][mx] <= 1:
+            check = True
+
     return check
 
 #プレイヤーを動かす
 def move_player():
     global player_x,player_y
     if key == "Up":
-        if check_wall(player_x,player_y,DIR_UP) == False:
-            player_y = player_y - 60
+        if check_wall(player_x,player_y,DIR_UP,20) == False:
+            player_y = player_y - 20
     if key == "Down":
-        if check_wall(player_x,player_y,DIR_DOWN) == False:
-            player_y = player_y + 60
+        if check_wall(player_x,player_y,DIR_DOWN,20) == False:
+            player_y = player_y + 20
     if key == "Left":
-        if check_wall(player_x,player_y,DIR_LEFT) == False:
-            player_x = player_x - 60
+        if check_wall(player_x,player_y,DIR_LEFT,20) == False:
+            player_x = player_x - 20
     if key == "Right":
-        if check_wall(player_x,player_y,DIR_RIGHT) == False:
-            player_x = player_x + 60
+        if check_wall(player_x,player_y,DIR_RIGHT,20) == False:
+            player_x = player_x + 20
 
 def main():
     global key,koff
     draw_screen()
     move_player()
-    if koff == True:
-        key = ""
-        koff = False
+    
     root.after(100,main)
 
     
